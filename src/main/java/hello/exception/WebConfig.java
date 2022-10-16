3c4fd932942f896883c25e4c435b395dcad266ae
jiaLEE <jia9510@naver.com>
2022-10-16 21:54:28 +0900

    Servlet Exception - Interceptor

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -1,10 +1,13 @@
 package hello.exception;
 
 import hello.exception.filter.LogFiter;
+import hello.exception.interceptor.LogInterceptor;
 import lombok.extern.java.Log;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -13,7 +16,20 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    /**
+     * spring Interceptor 등록 -> excludePath로 거른다.
+     */
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**"); // 오류페이지 경로를 exclude 함.
+    }
+
+    /**
+     * serlvet filter 등록 -> dispatcher Type으로 거른다.
+     */
+   // @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFiter());
